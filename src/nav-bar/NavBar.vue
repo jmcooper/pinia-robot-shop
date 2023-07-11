cac92634bf09d6137e9af9d4c817548d1a316f2b
Jim Cooper <jim@cooperplanet.com>
2023-07-11 14:31:13 -0600

    Cleanup nav bar

--- src/nav-bar/NavBar.vue
+++ src/nav-bar/NavBar.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="container">
-    <div class="left">
-      <img class="logo" src="./logo.png" alt="Logo" />
-      <router-link to="/catalog" routerLinkActive="active">Catalog</router-link>
-      <router-link to="/cart" routerLinkActive="active">Cart</router-link>
-      <router-link to="/search" routerLinkActive="active">Search</router-link>
-    </div>
+    <img class="logo" src="./logo.png" alt="Logo" />
+    <router-link to="/catalog" routerLinkActive="active">Catalog</router-link>
+    <router-link to="/search" routerLinkActive="active">Search</router-link>
+    <div><router-link to="/cart" routerLinkActive="active">Cart</router-link></div>
   </div>
 </template>
 
@@ -15,20 +13,14 @@
 <style scoped>
 .container {
   display: flex;
-  justify-content: space-between;
   font-size: 24px;
-  margin-bottom: 10px;
   margin: -3px -8px 0 -8px;
   padding: 0 8px 5px 8px;
   border-bottom: 2px solid #818285;
-}
-
-.left {
-  display: flex;
   align-items: center;
 }
 
-.left * {
+.container * {
   margin-right: 25px;
 }
 

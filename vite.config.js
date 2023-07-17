0dad1ded8c763999e9aa447e4c54972f369ff6de
Jim Cooper <jim@cooperplanet.com>
2023-07-17 16:23:09 -0600

    Remove proxy server config

--- vite.config.js
+++ vite.config.js
@@ -13,11 +13,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8081',
-      },
-    },
-  },
 })
